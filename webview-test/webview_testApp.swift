50cd8aabeaf6539d420724bca7886ed6525c7bea
Zhihao Cui <davidcui2@me.com>
2022-09-21 22:15:20 +0100

    Initial Commit

---
+++ webview-test/webview_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  webview_testApp.swift
+//  webview-test
+//
+//  Created by Zhihao Cui on 21/09/2022.
+//
+
+import SwiftUI
+
+@main
+struct webview_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
